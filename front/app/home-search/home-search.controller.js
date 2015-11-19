2a0583919b896b73df50faab48aabed4071b7b46
Lior Arbel <lliorarbell@gmail.com>
2015-11-19 22:49:56 +0200

    shit shit shit fuck

--- front/app/home-search/home-search.controller.js
+++ front/app/home-search/home-search.controller.js
@@ -37,7 +37,7 @@
         };
 
         vm.selectedItemChange = function (med) {
-            $location.path('/med/' + med.name);
+            $location.path('/med/' + med._id);
         };
 
         function query(item){
